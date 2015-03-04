77548cd6924826cf0728b2ad87b1f27ae7a67619
Steffen Kram <steffen.kram@uni-ulm.de>
2015-03-04 11:28:30 +0100

    Updated jQAssistant to version 1.0.0-RC1.

--- src/main/java/de/uniulm/iai/jqassistant/javasrc/plugin/model/JavaCompilationUnitDescriptor.java
+++ src/main/java/de/uniulm/iai/jqassistant/javasrc/plugin/model/JavaCompilationUnitDescriptor.java
@@ -1,8 +1,8 @@
 package de.uniulm.iai.jqassistant.javasrc.plugin.model;
 
-import com.buschmais.jqassistant.core.store.api.model.FileDescriptor;
 import com.buschmais.jqassistant.core.store.api.model.FullQualifiedNameDescriptor;
 import com.buschmais.jqassistant.core.store.api.model.NamedDescriptor;
+import com.buschmais.jqassistant.plugin.common.api.model.FileDescriptor;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Property;
 import com.buschmais.xo.neo4j.api.annotation.Relation.Outgoing;
